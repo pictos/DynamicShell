632760fd1bc1667e91e3018e6ee8d707250e1394
Pedro Jesus <pedro.pj.souza@hotmail.com>
2019-11-03 22:56:17 -0300

    Adicionar arquivos de projeto.

---
+++ DynamicShell/DynamicShell/AppShell.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DynamicShell
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+			var mainPage = new FlyoutItem
+			{
+				Title = "Main Page",
+				Items =
+				{
+					new ShellContent
+					{
+						Content = new MainPage()
+					}
+				}
+			};
+
+			var page1 = new FlyoutItem
+			{
+				Title = "Page 1",
+				Items =
+				{
+					new ShellContent
+					{
+						Content = new Page1()
+					}
+				}
+			};
+
+			Items.Add(mainPage);
+			Items.Add(page1);
+		}
+	}
+}
---
+++ DynamicShell/DynamicShell/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DynamicShell
+{
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		void Button_Clicked(object sender, EventArgs e)
+		{
+			var mainPage = new FlyoutItem
+			{
+				Title = "Main Page",
+				Items =
+				{
+					new ShellContent
+					{
+						Content = new MainPage()
+					}
+				}
+			};
+
+			var page1 = new FlyoutItem
+			{
+				Title = "Page 1",
+				Items =
+				{
+					new ShellContent
+					{
+						Content = new Page1()
+					}
+				}
+			};
+			Shell.Current.Items.Clear();
+			Shell.Current.Items.Add(page1);
+			Shell.Current.Items.Add(mainPage);
+		}
+	}
+}
